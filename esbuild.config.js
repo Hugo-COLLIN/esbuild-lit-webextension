720b29df286beea03205bc8ac4be59dddcac87bc
Hugo COLLIN <hugo.collin1@outlook.fr>
2024-08-24 15:09:16 +0200

    feat: watch mode

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,6 +1,4 @@
 const esbuild = require('esbuild');
-const fs = require('fs');
-const path = require('path');
 const {generateManifestPlugin} = require("./config/esbuild/plugins/generateManifestPlugin");
 const {generateAppInfosPlugin} = require("./config/esbuild/plugins/generateAppInfosPlugin");
 const {generateLicensesPlugin} = require("./config/esbuild/plugins/generateLicensesList");
@@ -10,30 +8,36 @@ const {copyStaticFilesPlugin} = require("./config/esbuild/plugins/copyStaticFile
 const targetBrowser = process.env.TARGET || 'chrome';
 const appMode = process.env.APP_MODE || 'dev';
 
-esbuild.build({
-  entryPoints: ['src/background.ts', 'src/my-element.ts', 'src/popup.html'],
-  bundle: true,
-  outdir: 'dist',
-  minify: false,
-  sourcemap: false,
-  target: ['chrome89', 'firefox89'],
-  format: 'esm',
-  logLevel: 'info',
-  loader: {
-    '.ts': 'ts',
-    '.html': 'copy',
-  },
-  entryNames: '[name]',
-  plugins: [
-    generateManifestPlugin(targetBrowser),
-    generateAppInfosPlugin(appMode),
-    generateLicensesPlugin(),
-    copyStaticFilesPlugin(['public']) //'src/manifest.json'
-  ]
-}).then(() => {
-  // fill .then will break generateLicensesPlugin generation (?)
-}).catch((error) => {
+async function build() {
+  const ctx = await esbuild.context({
+    entryPoints: ['src/background.ts', 'src/my-element.ts', 'src/popup.html'],
+    bundle: true,
+    outdir: 'dist',
+    minify: false,
+    sourcemap: false,
+    target: ['chrome89', 'firefox89'],
+    format: 'esm',
+    logLevel: 'info',
+    loader: {
+      '.ts': 'ts',
+      '.html': 'copy',
+    },
+    entryNames: '[name]',
+    plugins: [
+      generateManifestPlugin(targetBrowser),
+      generateAppInfosPlugin(appMode),
+      generateLicensesPlugin(),
+      copyStaticFilesPlugin(['public']),
+    ],
+  });
+
+  // Activate watch mode
+  await ctx.watch();
+  console.log('Watching for file changes...');
+}
+
+build().catch((error) => {
   console.error('Build failed:', error);
   console.error('Stack Trace:', error.stack);
-  process.exit(1)
+  process.exit(1);
 });
