a42d325712429bba8e6780f3137085493c28844c
Hugo COLLIN <hugo.collin1@outlook.fr>
2024-08-24 09:07:09 +0200

    update esbuild config

--- esbuild.config.js
+++ esbuild.config.js
@@ -3,9 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 // Copier les fichiers statiques (HTML, manifest)
-const copyStaticFiles = () => {
-  const staticFiles = ['src/manifest.json'];
-
+const copyStaticFiles = (staticFiles) => {
   staticFiles.forEach((file) => {
     const filePath = path.resolve(__dirname, file);
     const destPath = path.resolve(__dirname, 'dist', path.basename(file));
@@ -18,17 +16,17 @@ esbuild.build({
   entryPoints: ['src/background.ts', 'src/my-element.ts', 'src/popup.html'],
   bundle: true,
   outdir: 'dist',
-  minify: true,
+  minify: false,
   sourcemap: true,
   target: ['chrome89', 'firefox89'],
   format: 'esm',
   logLevel: 'info',
   loader: {
     '.ts': 'ts',
-    '.html': 'copy',  // On spécifie de copier le fichier HTML manuellement, donc on utilise 'copy'
+    '.html': 'copy',
   },
-  entryNames: '[name]',  // Garder le même nom de fichier sans hash
+  entryNames: '[name]',  // Keep the same file name without hash
 }).then(() => {
   // Copier les fichiers statiques après la compilation
-  copyStaticFiles();
+  copyStaticFiles('src/manifest.json');
 }).catch(() => process.exit(1));
