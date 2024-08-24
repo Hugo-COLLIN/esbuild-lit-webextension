6a6ada284040422cddbbd4ae833e9f8183a2ae48
Hugo COLLIN <hugo.collin1@outlook.fr>
2024-08-24 19:36:56 +0200

    chore: rename assets into dist

--- src/background.ts
+++ src/background.ts
@@ -1,1 +1,3 @@
 console.log("Extension background script running.");
+console.log("This script is loaded every time the browser is open.");
+console.log("This script is loaded every time the browser is open.");
