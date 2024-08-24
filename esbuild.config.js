a348a772a580899a7429cbed6e557d83a87ce566
Hugo COLLIN <hugo.collin1@outlook.fr>
2024-08-24 15:33:08 +0200

    feat: differentiate build/watch modes

--- esbuild.config.js
+++ esbuild.config.js
@@ -4,40 +4,47 @@ const {generateAppInfosPlugin} = require("./config/esbuild/plugins/generateAppIn
 const {generateLicensesPlugin} = require("./config/esbuild/plugins/generateLicensesList");
 const {copyStaticFilesPlugin} = require("./config/esbuild/plugins/copyStaticFilesPlugin");
 
-// Détecter le navigateur cible
 const targetBrowser = process.env.TARGET || 'chrome';
 const appMode = process.env.APP_MODE || 'dev';
+const watchMode = process.env.WATCH_MODE || false; // Flag for watch mode
 
-async function build() {
-  const ctx = await esbuild.context({
-    entryPoints: ['src/background.ts', 'src/my-element.ts', 'src/popup.html'],
-    bundle: true,
-    outdir: 'dist',
-    minify: false,
-    sourcemap: false,
-    target: ['chrome89', 'firefox89'],
-    format: 'esm',
-    logLevel: 'info',
-    loader: {
-      '.ts': 'ts',
-      '.html': 'copy',
-    },
-    entryNames: '[name]',
-    plugins: [
-      generateManifestPlugin(targetBrowser),
-      generateAppInfosPlugin(appMode),
-      generateLicensesPlugin(),
-      copyStaticFilesPlugin(['public']),
-    ],
-  });
+const options = {
+  entryPoints: ['src/background.ts', 'src/my-element.ts', 'src/popup.html'],
+  bundle: true,
+  outdir: 'dist',
+  minify: false,
+  sourcemap: false,
+  target: ['chrome89', 'firefox89'],
+  format: 'esm',
+  logLevel: 'info',
+  loader: {
+    '.ts': 'ts',
+    '.html': 'copy',
+  },
+  entryNames: '[name]',
+  plugins: [
+    generateManifestPlugin(targetBrowser),
+    generateAppInfosPlugin(appMode),
+    generateLicensesPlugin(),
+    copyStaticFilesPlugin(['public']),
+  ],
+};
 
-  // Activate watch mode
-  await ctx.watch();
-  console.log('Watching for file changes...');
-}
+const job = watchMode ? watch : build;
 
-build().catch((error) => {
+job().catch((error) => {
   console.error('Build failed:', error);
   console.error('Stack Trace:', error.stack);
   process.exit(1);
 });
+
+async function watch() {
+  const ctx = await esbuild.context(options);
+  await ctx.watch();
+  console.log('Watching for file changes...');
+}
+
+async function build() {
+  await esbuild.build(options);
+  console.log('Build completed successfully');
+}
