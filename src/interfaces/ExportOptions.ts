14b9c4f79af033b5c666ee51946abdf55afbd8c7
Hugo COLLIN <hugo.collin1@outlook.fr>
2024-08-26 22:43:08 +0200

    refactor: cleaning code

--- src/interfaces/ExportOptions.ts
+++ src/interfaces/ExportOptions.ts
@@ -1,6 +1,5 @@
 import { LitElement, html, css } from 'lit';
 import { customElement, state } from 'lit/decorators.js';
-// import '../scripts/pages/shoelace-styles';
 import '@shoelace-style/shoelace/dist/components/button/button.js';
 import '@shoelace-style/shoelace/dist/components/input/input.js';
 import '@shoelace-style/shoelace/dist/components/alert/alert.js';
@@ -37,7 +36,7 @@ export class ExportOptions extends LitElement {
           align-items: center;
           justify-content: center;
       }
-      
+
       #options-fieldset {
           padding: 0 0 1rem 0;
           width: 100%;
